582e8d49e332c7175a5db4c7f923a5095692315f
yuehuafei <1490165179@qq.com>
2017-06-15 18:13:03 +0800

    Update person.js

--- main/person.js
+++ main/person.js
@@ -2,5 +2,5 @@
 module.exports = Person(name,age){
   this.name=name;
   this.age=age;
-  console.log("My name is"+name+". I am "+age+" years old.");
+  console.log("My name is "+name+". I am "+age+" years old.");
 }
