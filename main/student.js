d3aefcc58a1791a5a1e73fcf6f1621c46649d6d5
yuehuafei <1490165179@qq.com>
2017-06-15 18:12:15 +0800

    Create student.js

--- main/student.js
+++ main/student.js
@@ -1,3 +1,7 @@
 let Person = require('./person')
 // Write your code here
-module.exports = Student
+module.exports = Student(name,age,grade){
+  Person.call(this,name,age);
+  this.grade=grade;
+  console.log("My name is "+name+". I am "+age+" years old. I am a Student. I am at Class "+grade);
+}
