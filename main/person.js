b4ba92404bec8e5a1c8e3daf4e69272529b10265
yuehuafei <1490165179@qq.com>
2017-06-15 17:57:41 +0800

    Create person.js

--- main/person.js
+++ main/person.js
@@ -1,2 +1,6 @@
 // Write your code here
-module.exports = Person
+module.exports = Person(name,age){
+  this.name=name;
+  this.age=age;
+  console.log("My name is"+name+". I am "+age+" years old.");
+}
