b3d8a9fe4de7a3089e4131056e985a50da5f2238
yuehuafei <1490165179@qq.com>
2017-06-16 09:31:57 +0800

    Update person.js

--- main/person.js
+++ main/person.js
@@ -1,8 +1,9 @@
 // Write your code here
-module.exports = Person(name,age){
-  this.name=name;
-  this.age=age;
+module.exports = Person(n,a){
+  var name=n;
+  var age=a;
+ public var introduce(){
+   return ("My name is "+name+". I am "+age+" years old.");
+ }
 }
-public var introduce(){
- return ("My name is "+name+". I am "+age+" years old.");
-}
+
